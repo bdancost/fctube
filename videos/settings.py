0bb782836e51a3615addcba17396ac9bdf3c5958
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-10-25 16:49:53 -0300

    Novas funcionalidades conexao ao banco de dados e correçao de bugs

--- videos/settings.py
+++ videos/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = "videos.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   # "default": {
+   #     "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR / "db.sqlite3",
+   # }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'mydb',
+       'USER': 'postgres',
+       'PASSWORD': 'Dev2024full',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
